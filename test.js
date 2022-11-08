d57eb4c7293ed097634345f99d4c9a4f8babe613
Lucas Barrena <lucasbarrena98@gmail.com>
2022-11-08 15:50:32 -0300

    Update test.js

--- test.js
+++ test.js
@@ -391,6 +391,43 @@ test('deduplicate muxers', function (t) {
   bbar.addMessage({ encoding: c.string }).send('hello bar')
 })
 
+test.solo('open + send + close on same tick', async function (t) {
+  t.plan(4)
+
+  const a = new Protomux(new SecretStream(true))
+  const b = new Protomux(new SecretStream(false))
+
+  replicate(a, b)
+
+  const ac = a.createChannel({
+    protocol: 'foo',
+    onopen () {
+      t.pass('a opened')
+    },
+    onclose () {
+      t.pass('a closed')
+    }
+  })
+
+  ac.open()
+  ac.addMessage({ encoding: c.string, onmessage: (message) => t.is(message, 'hello') })
+  // ac.close()
+
+  const bc = b.createChannel({
+    protocol: 'foo',
+    onopen () {
+      t.pass('b opened')
+    },
+    onclose () {
+      t.pass('b closed')
+    }
+  })
+
+  bc.open()
+  ac.addMessage({ encoding: c.string }).send('hello')
+  bc.close()
+})
+
 function replicate (a, b) {
   a.stream.rawStream.pipe(b.stream.rawStream).pipe(a.stream.rawStream)
 }
