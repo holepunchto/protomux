de78bd57c30d16fa68907062689c53ac17172c07
HDegroote <75906619+HDegroote@users.noreply.github.com>
2024-05-01 09:39:22 +0200

    Add hypertrace for recv and send methods in Channel (#18)
    * Add hypertrace for recv and send methods in Channel
    
    * Use existing self variable (s)
    
    * Order tracer props hierarchically

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const b4a = require('b4a')
 const c = require('compact-encoding')
 const queueTick = require('queue-tick')
 const safetyCatch = require('safety-catch')
+const { createTracer } = require('hypertrace')
 
 const MAX_BUFFERED = 32768
 const MAX_BACKLOG = Infinity // TODO: impl "open" backpressure
@@ -9,6 +10,8 @@ const MAX_BATCH = 8 * 1024 * 1024
 
 class Channel {
   constructor (mux, info, userData, protocol, aliases, id, handshake, messages, onopen, onclose, ondestroy, ondrain) {
+    this.tracer = createTracer(this)
+
     this.userData = userData
     this.protocol = protocol
     this.aliases = aliases
@@ -199,10 +202,22 @@ class Channel {
       encoding,
       onmessage,
       recv (state, session) {
+        s.tracer.trace('recv', {
+          protocol: s.protocol,
+          id: s.id,
+          type
+        })
+
         return session._track(m.onmessage(encoding.decode(state), session))
       },
       send (m, session = s) {
         if (session.closed === true) return false
+
+        s.tracer.trace('send', {
+          protocol: s.protocol,
+          id: s.id,
+          type
+        })
 
         const mux = session._mux
         const state = { buffer: null, start: 0, end: typeLen }
